569f1a5a0a80e76781a009ec17d37eb666406211
honorkin <imdave@naver.com>
2019-10-21 01:47:32 +0900

    1020 0:21

--- app/src/main/java/com/example/baccaratmaybenextv1/MainActivity.kt
+++ app/src/main/java/com/example/baccaratmaybenextv1/MainActivity.kt
@@ -9,12 +9,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.InterstitialAd
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mAdView : AdView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+      override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
--- app/src/main/java/com/example/baccaratmaybenextv1/SubActivity.kt
+++ app/src/main/java/com/example/baccaratmaybenextv1/SubActivity.kt
@@ -2,19 +2,41 @@ package com.example.baccaratmaybenextv1
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
+import com.google.android.gms.ads.MobileAds
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_sub.*
 
 class SubActivity : AppCompatActivity() {
 
+    private lateinit var mInterstitialAd: InterstitialAd
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
 
+        MobileAds.initialize(this,
+            "ca-app-pub-3940256099942544~3347511713")
+        mInterstitialAd = InterstitialAd(this)
+        mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
+
+
         btn2.setOnClickListener {
+
+
+
             var intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+
+            if (mInterstitialAd.isLoaded) {
+                mInterstitialAd.show()
+            } else {
+                Log.d("TAG", "The interstitial wasn't loaded yet.")
+            }
         }
 
     }
